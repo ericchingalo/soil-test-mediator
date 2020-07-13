b0319e062df56ab6c4f3a8342baf29230a23ed27
chingaloEric <echingalo@gmail.com>
2020-07-13 19:05:34 +0300

    feat: 🎸 added database connectiong and hosting

--- functions/index.js
+++ functions/index.js
@@ -11,9 +11,26 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://soil-test-api.firebaseio.com',
 });
+const db = admin.firestore();
 
-app.get('/hello-world', (req, res) => {
-  return res.status(200).send('Hello World!');
+app.get('/api', (req, res) => {
+  return res.status(200).send('Hello, Welcome to Soil test mediator api!');
+});
+
+// create
+app.post('/api/create', (req, res) => {
+  (async () => {
+    try {
+      await db
+        .collection('items')
+        .doc('/' + req.body.id + '/')
+        .create({ item: req.body.item });
+      return res.status(200).send();
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
 });
 
 exports.app = functions.https.onRequest(app);
